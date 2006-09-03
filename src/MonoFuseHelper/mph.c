ef7edb33f6316126a36e4b606cea3005cfe71ef8
Jonathan Pryor <jon@balthasar.jprl.com>
2006-09-03 08:00:04 -0400

    Add license information (MIT/X11) to COPYING and source code.
    	* COPYING: Specify license as MIT/X11.
    	* example/HelloFS/HelloFS.cs, src/Mono.Fuse/FileSystem.cs,
    	  src/MonoFuseHelper/mfh.c, src/MonoFuseHelper/mph.c: Add license.

--- src/MonoFuseHelper/mph.c
+++ src/MonoFuseHelper/mph.c
@@ -1,8 +1,34 @@
 /*
- * MonoPosixHelper transplanted functions.
+ * mph.c: MonoPosixHelper transplanted functions.  
+ *        Useful for when running on an older version of Mono.
  *
- * Useful for when running on an older version of Mono.
+ * Authors:
+ *   Jonathan Pryor  (jonpryor@vt.edu)
+ *
+ * Copyright (C) 2006 Jonathan Pryor
  */
+
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef HAVE_MONO_UNIX_NATIVE_COPY_FUNCS
 
 #include <config.h>
@@ -126,4 +152,5 @@ Mono_Fuse_FromStatvfs (struct Mono_Unix_Native_Statvfs *from, void *_to)
 	return 0;
 }
 
-#endif
+#endif /* ndef HAVE_MONO_UNIX_NATIVE_COPY_FUNCS */
+
