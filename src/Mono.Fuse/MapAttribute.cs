512a3522d052b73541588054992336b5e550a434
Jonathan Pryor <jon@balthasar.magi>
2007-04-12 14:10:34 -0400

    Mac OS X + MacFUSE compilation and runtime fixes.
      * src/Mono.Fuse/create-native-map.exe, src/Mono.Fuse/MapAttribute.cs: Update
        to latest versions (fixes buggy overflow checking in MonoFuseHelper).
      * lib/Mono.Fuse/Makefile.am: FUSE_USE_VERSION should be in the .c file, not
        the .h file.  Also fixes Mac OS X compilation, as FUSE_USE_VERSION needs
        to be defined *before* including <fuse.h>, but <fuse.h> is inserted before
        the include of lib/map.h -- bad order.

--- src/Mono.Fuse/MapAttribute.cs
+++ src/Mono.Fuse/MapAttribute.cs
@@ -37,6 +37,7 @@ using System;
 		AttributeTargets.Struct)]
 internal class MapAttribute : Attribute {
 	private string nativeType;
+	private string suppressFlags;
 
 	public MapAttribute ()
 	{
@@ -49,6 +50,11 @@ internal class MapAttribute : Attribute {
 
 	public string NativeType {
 		get {return nativeType;}
+	}
+
+	public string SuppressFlags {
+		get {return suppressFlags;}
+		set {suppressFlags = value;}
 	}
 }
 
